40c0cbb2ca2f239c3eba3b8f37b80f947056f6ce
kumarakshay2023 <53837497+kumarakshay2023@users.noreply.github.com>
2023-09-21 11:47:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const user = [
+    {
+        "name":"Akshay",
+        "roll":29
+    },
+    {
+        "name":"hellow",
+        "roll":22
+    },
+    {
+        "name":"Abho",
+        "roll":25
+    }
+]
+
+app.get('/user',(req,res)=>{
+    res.send({
+        msg:"List of user",
+        success:true,
+        data:user
+    })
+});
+
+
+
+app.listen(8080,()=>{
+    console.log("listing to port 8080");
+})
